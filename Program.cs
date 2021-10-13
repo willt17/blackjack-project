915d42c145b337f00b1560a5c9810eafa4e46be4
Will Tidwell <jwtidwell1@crimson.ua.edu>
2021-10-13 10:26:59 -0500

    Added first hand logic.

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace blackjack_project
         static void Main(string[] args)
         {
             Console.WriteLine( "Time to play blackjack" );
+            blackjackGame();
         }
         static int random2()
         {
@@ -14,6 +15,22 @@ namespace blackjack_project
             int number;
             number = randomNumber.Next( 1, 10 );
             return number;
+        }
+        static void blackjackGame()
+        {
+            int player1Cards = 0;
+            int player2Cards = 0;
+            if ( player1Cards < 21 && player2Cards < 21 )
+            {
+                bjPlayerTurnInit( ref player1Cards );
+            }
+        }
+        static void bjPlayerTurnInit( ref int player1Cards )
+        {
+            int pCard1 = random2();
+            int pCard2 = random2();
+            int pHand = pCard1 + pCard2;
+            Console.WriteLine( pHand );
         }
     }
 }

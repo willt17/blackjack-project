c832e18f1b68cd4de15c5999f97be00feb43983d
Will Tidwell <jwtidwell1@crimson.ua.edu>
2021-10-13 11:01:01 -0500

    Added second hand choice.

--- Program.cs
+++ Program.cs
@@ -23,14 +23,53 @@ namespace blackjack_project
             if ( player1Cards < 21 && player2Cards < 21 )
             {
                 bjPlayerTurnInit( ref player1Cards );
+                bjComputerTurnInit( ref player2Cards );
+                Console.WriteLine( "You have " + player1Cards );
+                Console.WriteLine( "The computer has " + player2Cards );
+                playerDecision();
             }
         }
         static void bjPlayerTurnInit( ref int player1Cards )
         {
             int pCard1 = random2();
             int pCard2 = random2();
-            int pHand = pCard1 + pCard2;
-            Console.WriteLine( pHand );
+            player1Cards  = pCard1 + pCard2;  
+        }
+        static void bjComputerTurnInit( ref int player2Cards )
+        {
+            int cCard1 = random2();
+            int cCard2 = random2();
+            player2Cards = cCard1 + cCard2; 
+        }
+        static void playerDecision()
+        {
+            Console.WriteLine( "Press 1 to stand or 2 to hit" );
+            string input = Console.ReadLine();
+            int userInput = int.Parse( input );
+            switch ( userInput )
+            {
+                case 1:
+                    standFunction();
+                    break;
+                case 2:
+                    hitFunction();
+                    break;
+                default:
+                    wrongInput();
+                    break;
+            }
+        }
+        static void standFunction()
+        {
+            Console.WriteLine( "You chose to stand" );
+        }
+        static void hitFunction()
+        {
+            Console.WriteLine( "You chose to hit" );
+        }
+        static void wrongInput()
+        {
+            Console.WriteLine( "That input is not valid" );
         }
     }
 }
